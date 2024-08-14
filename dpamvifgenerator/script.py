fd1297f530ad84ef87b349023bd0badd5138fa50
Ayush <amishra@vtmgroup.com>
2024-08-14 11:03:00 -0700

    Fixed DPAM VIF generation issue for products with no ports (Cable, etc.)

--- dpamvifgenerator/script.py
+++ dpamvifgenerator/script.py
@@ -132,8 +132,10 @@ class DPAMVIFGenerator:
     @staticmethod
     def load_input_vif(in_vif: str) -> ET:
         try:
+            # Initalizing parser outside the call helps parse all "utf-8" characters (done specifically for the registered trademark character)
+            parser = ET.XMLParser(target=ET.TreeBuilder(insert_comments=True), encoding='utf-8')
             return ET.parse(
-                in_vif, parser=ET.XMLParser(target=ET.TreeBuilder(insert_comments=True))
+                in_vif, parser=parser
             )
         except Exception as e:
             error = (
@@ -146,8 +148,9 @@ class DPAMVIFGenerator:
     @staticmethod
     def load_dpam_settings(settings: str) -> ET:
         try:
+            parser = ET.XMLParser(target = ET.TreeBuilder(insert_comments=True), encoding='utf-8')
             return ET.parse(
-                settings, parser=ET.XMLParser(target=ET.TreeBuilder(insert_comments=True))
+                settings, parser=parser
             )
         except Exception as e:
             error = (
@@ -159,23 +162,89 @@ class DPAMVIFGenerator:
             raise InvalidSettingsXML(error)
 
     @staticmethod
+    def elements_equal(e1, e2):
+        #Check if the contents of two elements are exactly the same
+        if e1.tag != e2.tag: return False
+        if e1.text != e2.text: return False
+        if e1.tail != e2.tail: return False
+        if e1.attrib != e2.attrib: return False
+        if len(e1) != len(e2): return False
+        return all(DPAMVIFGenerator.elements_equal(c1, c2) for c1, c2 in zip(e1, e2))
+    
+    @staticmethod
+    def contains_contents(obj1, obj2):
+        # Check if tags are the same
+        if obj1.tag != obj2.tag:
+            return False
+
+        # Check if obj2 is a direct child of obj1
+        if obj2 in obj1:
+            return True
+
+        # Check if obj2 is a subset of any child of obj1
+        for child in obj1:
+            if child.tag == '{http://usb.org/VendorInfoFileOptionalContent.xsd}OptionalContent':
+
+                if DPAMVIFGenerator.contains_contents(child, obj2):
+                    return True
+                else:
+                    xml_string1 = ET.tostring(child, encoding='unicode', method='xml')
+                    xml_string2 = ET.tostring(obj2, encoding='unicode', method='xml')
+
+                    # Split the strings into lines
+                    lines1 = xml_string1.splitlines()
+                    lines2 = xml_string2.splitlines()
+
+                    # Strip leading/trailing whitespace from each line
+                    lines1 = [line.strip() for line in lines1]
+                    lines2 = [line.strip() for line in lines2]
+
+                    return lines1 == lines2
+
+        return False
+
+    @staticmethod
     def generate_dpam_vif(input_vif: ET, dpam_settings: ET):
         # Get port DPAM settings from DPAM Settings XML
         port_settings = DPAMVIFGenerator.get_port_settings_from_vif(dpam_settings)
 
         # Insert DPAM Opt Content blocks on each port
         prefix_map = DPAMVIFGenerator.get_prefix_map()
+
         for port in input_vif.getroot().findall(".//vif:Component", prefix_map):
-            port_name = port.find("vif:Port_Label", prefix_map).text
+
+            #Getting port_name element from the file
+            port_name = port.find("vif:Port_Label", prefix_map)
+
             # Check for existing optional content
             optional_content = port.find("opt:OptionalContent", prefix_map)
-            if optional_content:
-                # Merge DPAM opt content since OptionalContent block already exists
-                optional_content.append(port_settings[port_name])
+
+            # If both OptionalContent and Port_Name exist
+            if optional_content and port_name != None:
+
+                if(not DPAMVIFGenerator.contains_contents(optional_content, port_settings[port_name.text])):
+                    # Merge DPAM opt content since OptionalContent block already exists
+                    optional_content.append(port_settings[port_name.text])
             else:
-                # No existing OptionalContent, so use DPAM version as is
-                port.append(ET.Comment("Non-USB Content"))
-                port.append(port_settings[port_name])
+                
+                # Optional Content not found
+                if port_name != None:
+                    port.append(ET.Comment("Non-USB Content"))
+                    port.append(port_settings[port_name.text])
+
+                # No Port Name Found
+                else:
+                    # If Optional Content is None, just add the Optional Content as is
+                    if(optional_content is None):
+                        port.append(ET.Comment("Non-USB Content"))
+                        port.append(port_settings['NA'])
+
+                    # Optinal Content already exists and
+                    elif(optional_content is not None and not DPAMVIFGenerator.elements_equal(optional_content, port_settings["NA"])):
+                        port.append(ET.Comment("Non-USB Content"))
+                        port.append(port_settings['NA'])
+                    else:
+                        logging.info('Same OptionalContent already found in the VIF')
 
     @staticmethod
     def get_port_settings_from_vif(dpam_settings: ET) -> dict[str, ET.Element]:
@@ -183,12 +252,15 @@ class DPAMVIFGenerator:
         prefix_map = DPAMVIFGenerator.get_prefix_map()
         port_settings: dict[str, ET.Element] = {}
         for port in dpam_settings.getroot().findall(".//vif:Component", prefix_map):
-            port_name = port.find("vif:Port_Label", prefix_map).text
-            if not port_name:
-                error = "Error: Missing vif:Port_Label from DPAM Settings XML file"
-                logging.error(error)
-                raise InvalidSettingsXML(error)
-            port_settings[port_name] = port.find(".//opt:OptionalContent", prefix_map)
+            
+            port_data = port.find("vif:Port_Label", prefix_map)
+
+            if(port_data != None):
+                port_name = port_data.text
+                port_settings[port_name] = port.find(".//opt:OptionalContent", prefix_map)
+            else:
+                # If no port is found we still associate the settings to a "NA" port_name to still go through with generating a DPAM VIF File.
+                port_settings['NA'] = port.find(".//opt:OptionalContent", prefix_map)
         return port_settings
 
     @staticmethod
